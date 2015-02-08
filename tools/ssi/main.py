52e90fbda29cf027aa83c3cd6354c36d3f871884
shiro <shiro@worldofcorecraft.com>
2015-02-09 00:18:03 +0100

    Mask segfault on exit
    Not a pretty solution, but the non-deterministic order of destruction caused
    QCompleter to make the program segfault on exit. We mask that by using
    sip.setdestroyonexit(False).

--- tools/ssi/main.py
+++ tools/ssi/main.py
@@ -4,13 +4,14 @@ import main_window
 from PyQt4 import QtGui
 import os
 import sys
+import sip
+
+app = QtGui.QApplication(sys.argv)
+wnd = main_window.main_window()
 
 def main():
-    app = QtGui.QApplication(sys.argv)
-
-    wnd = main_window.main_window()
+    sip.setdestroyonexit(False)
     wnd.show()
-
     sys.exit(app.exec())
 
 if __name__ == '__main__':
